86eeb9692bfe0b3466669c694d13a789d4a726ba
Andydemarks <andydemarks@andydemarks.com>
2022-02-17 14:17:21 -0500

    updated some appsettings json

--- PlanetPaintballApi/Program.cs
+++ PlanetPaintballApi/Program.cs
@@ -1,3 +1,5 @@
+using PPDL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IRepository>(repo => new SQLRepository());
 
 var app = builder.Build();
 
