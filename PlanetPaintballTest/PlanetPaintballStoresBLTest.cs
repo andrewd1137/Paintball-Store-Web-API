6203b17d9ee5134a4932913f0b203db23b61fa51
Andydemarks <andydemarks@andydemarks.com>
2022-02-24 15:04:31 -0500

    added new storesbl test

--- PlanetPaintballTest/PlanetPaintballStoresBLTest.cs
+++ PlanetPaintballTest/PlanetPaintballStoresBLTest.cs
@@ -160,6 +160,25 @@ namespace PlanetPaintballTest
 
         }
 
+        [Fact]
+        public void ShouldViewOrderHistory()
+        {
+            int testOrderId = 1;
+            int testProductID = 1;
+            
+            Products product = new Products()
+            {
+                ID = testProductID
+            };
+
+            Mock<IRepository> mockRepo = new Mock<IRepository>();
+
+            mockRepo.Setup(repo => repo.ViewOrder(product));
+
+            IPlanetPaintballStoresBL planetPaintballStoresBL = new PlanetPaintballStoresBL(mockRepo.Object);
+
+        }
+
 
     }
 
